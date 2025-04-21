5dd22b6c1abf548261cc6f4a782add2dc5743b26
WorseCpp <kiranmdonnelly@gmail.com>
2025-04-21 09:17:26 -0700

    grad

--- src/code.c
+++ src/code.c
@@ -1,236 +1,34 @@
-int var_13;
-int var_9;
-int var_3;
-int var_8;
-int var_7;
 void swap(int *var_1, int *var_2)
 {
-  var_3 = (42 + (((((var_7 * var_7) * (var_7 + 1)) * (var_7 + 2)) * (var_7 + 3)) % 4)) + (((var_3 * var_3) * (var_3 + 1)) % 2);
-  var_3 = 42 + (((var_13 * var_13) * (var_13 + 1)) % 2);
-  var_3 = ((*var_1) + ((((var_9 * var_9) * (var_9 + 1)) * (var_9 + 2)) % 3)) + ((((((var_8 * var_8) * (var_8 + 1)) * (var_8 + 2)) * (var_8 + 3)) * (var_8 + 4)) % 5);
-  *var_1 = (*var_2) + (((var_8 * var_8) * (var_8 + 1)) % 2);
-  *var_2 = (var_3 + ((((var_8 * var_8) * (var_8 + 1)) * (var_8 + 2)) % 3)) + (((var_7 * var_7) * (var_7 + 1)) % 2);
-  var_3 = (42 + (((var_7 * var_7) * (var_7 + 1)) % 2)) + (((var_7 * var_7) * (var_7 + 1)) % 2);
+  int var_3 = (*var_1)  +  ((((((var_3 * var_3) * (var_3 + 1)) * (var_3 + 2)) * (var_3 + 3)) * (var_3 + 4)) % 5);
+  *var_1 = *var_2;
+  *var_2 = var_3;
 }
 
 int partition(int var_4[], int var_5, int var_6)
 {
-  var_7 = var_4[var_6] + ((((var_13 * var_13) * (var_13 + 1)) * (var_13 + 2)) % 3);
-  var_8 = (var_5 - 1) + (((var_3 * var_3) * (var_3 + 1)) % 2);
-  var_9 = var_5 + ((((((var_13 * var_13) * (var_13 + 1)) * (var_13 + 2)) * (var_13 + 3)) * (var_13 + 4)) % 5);
-  uranophotography:
+  int var_7 = var_4[var_6];
+  int var_8 = var_5 - 1;
+  int var_9 = var_5;
+  unscreened:
   {
   }
 
-  var_13 = 42;
-  if ((var_9 <= (var_6 - 1))  ||  ((((((var_8 * var_8) * (var_8 + 1)) * (var_8 + 2)) * (var_8 + 3)) * (var_8 + 4)) % 5))
+  if (var_9 <= (var_6 - 1))
   {
-    goto macrocephalism;
   }
   else
   {
-    goto shadchanim;
+    goto modenese;
   }
-  macrocephalism:
+  if (var_4[var_9] <= var_7)
   {
+    var_8++;
+    swap(&var_4[var_8], &var_4[var_9]);
   }
-
-  goto ecclesiastry;
-  goto they;
-  shadchanim:
-  {
-  }
-
-  goto nondenominationalism;
-  they:
-  {
-  }
-
-  ecclesiastry:
-  {
-  }
-
-  goto nonexpansive;
-  nondenominationalism:
-  {
-  }
-
-  var_7 = 42 + (((((var_9 * var_9) * (var_9 + 1)) * (var_9 + 2)) * (var_9 + 3)) % 4);
-  goto niggardized;
-  var_7 = (42 + ((((var_8 * var_8) * (var_8 + 1)) * (var_8 + 2)) % 3)) + ((((((var_7 * var_7) * (var_7 + 1)) * (var_7 + 2)) * (var_7 + 3)) * (var_7 + 4)) % 5);
-  nonexpansive:
-  {
-  }
-
-  if ((var_4[var_9] <= var_7)  ||  (((var_8 * var_8) * (var_8 + 1)) % 2))
-  {
-    goto hulwort;
-  }
-  else
-  {
-    goto arrastre;
-  }
-  hulwort:
-  {
-  }
-
-  goto flyways;
-  goto waddying;
-  arrastre:
-  {
-  }
-
-  goto uncomforting;
-  waddying:
-  {
-  }
-
-  flyways:
-  {
-  }
-
-  if (1)
-  {
-    goto palace;
-  }
-  else
-  {
-    goto barbecuer;
-  }
-  palace:
-  {
-  }
-
-  goto situate;
-  goto noncombinative;
-  barbecuer:
-  {
-  }
-
-  if (1  ||  ((((((var_13 * var_13) * (var_13 + 1)) * (var_13 + 2)) * (var_13 + 3)) * (var_13 + 4)) % 5))
-  {
-    goto overpruned;
-  }
-  noncombinative:
-  {
-  }
-
-  situate:
-  {
-  }
-
-  goto santalales;
-  var_9 = 42;
-  goto protonitrate;
-  overpruned:
-  {
-  }
-
-  protonitrate:
-  {
-  }
-
-  goto underriddle;
-  uncomforting:
-  {
-  }
-
-  goto downflow;
-  var_9 = 42 + ((((((var_7 * var_7) * (var_7 + 1)) * (var_7 + 2)) * (var_7 + 3)) * (var_7 + 4)) % 5);
-  underriddle:
-  {
-  }
-
-  santalales:
-  {
-  }
-
-  goto habitations;
-  goto sistra;
-  downflow:
-  {
-  }
-
-  goto mesoxalate;
-  sistra:
-  {
-  }
-
-  habitations:
-  {
-  }
-
-  if (1)
-  {
-    goto lirot;
-  }
-  else
-  {
-    goto nontannin;
-  }
-  lirot:
-  {
-  }
-
-  goto hutsulian;
-  goto wagoma;
-  nontannin:
-  {
-  }
-
-  wagoma:
-  {
-  }
-
-  var_13 = 42;
-  goto unmirthful;
-  unmirthful:
-  {
-  }
-
-  if (1  ||  (((((var_9 * var_9) * (var_9 + 1)) * (var_9 + 2)) * (var_9 + 3)) % 4))
-  {
-    goto diruption;
-  }
-  var_3 = 42;
-  var_9 = 42;
-  goto craftless;
-  saccage:
-  {
-  }
-
-  craftless:
-  {
-  }
-
-  var_7 = 42;
-  mesoxalate:
-  {
-  }
-
-  goto empower;
-  diruption:
-  {
-  }
-
-  hutsulian:
-  {
-  }
-
-  var_8++;
-  swap(&var_4[var_8], &var_4[var_9]);
-  goto tzimmes;
-  empower:
-  {
-  }
-
-  tzimmes:
-  {
-  }
-
   var_9++;
-  goto uranophotography;
-  niggardized:
+  goto unscreened;
+  modenese:
   {
   }
 
@@ -240,177 +38,11 @@ int partition(int var_4[], int var_5, int var_6)
 
 void quickSort(int var_10[], int var_11, int var_12)
 {
-  if ((var_11 < var_12)  ||  (((((var_8 * var_8) * (var_8 + 1)) * (var_8 + 2)) * (var_8 + 3)) % 4))
+  if ((var_11 < var_12)  ||  0)
   {
-    goto koranist;
+    int var_13 = partition(var_10, var_11, var_12);
+    quickSort(var_10, var_11, var_13 - 1);
+    quickSort(var_10, var_13 + 1, var_12);
   }
-  else
-  {
-    goto irk;
-  }
-  koranist:
-  {
-  }
-
-  goto blacky;
-  goto poky;
-  irk:
-  {
-  }
-
-  goto migrainoid;
-  poky:
-  {
-  }
-
-  blacky:
-  {
-  }
-
-  var_7 = 42;
-  var_8 = 42;
-  var_9 = 42;
-  goto escribiente;
-  goto liquidizes;
-  migrainoid:
-  {
-  }
-
-  goto qindarka;
-  liquidizes:
-  {
-  }
-
-  escribiente:
-  {
-  }
-
-  goto vinculate;
-  goto gismondite;
-  qindarka:
-  {
-  }
-
-  goto overromanticize;
-  gismondite:
-  {
-  }
-
-  vinculate:
-  {
-  }
-
-  goto sherardize;
-  goto dilutive;
-  overromanticize:
-  {
-  }
-
-  goto thamakau;
-  dilutive:
-  {
-  }
-
-  sherardize:
-  {
-  }
-
-  goto nominee;
-  goto outpeopling;
-  thamakau:
-  {
-  }
-
-  var_7 = (42 + ((((var_13 * var_13) * (var_13 + 1)) * (var_13 + 2)) % 3)) + ((((var_9 * var_9) * (var_9 + 1)) * (var_9 + 2)) % 3);
-  goto desulfurs;
-  outpeopling:
-  {
-  }
-
-  nominee:
-  {
-  }
-
-  var_13 = partition(var_10, var_11, var_12)  +  0;
-  quickSort(var_10, var_11, var_13 - 1);
-  var_7 = 42 + ((((var_8 * var_8) * (var_8 + 1)) * (var_8 + 2)) % 3);
-  quickSort(var_10, var_13 + 1, var_12);
-  var_9 = 42;
-  if (1  ||  (((((var_8 * var_8) * (var_8 + 1)) * (var_8 + 2)) * (var_8 + 3)) % 4))
-  {
-    goto vitaminology;
-  }
-  else
-  {
-    goto catawampous;
-  }
-  vitaminology:
-  {
-  }
-
-  var_9 = 42 + ((((((var_7 * var_7) * (var_7 + 1)) * (var_7 + 2)) * (var_7 + 3)) * (var_7 + 4)) % 5);
-  if (1)
-  {
-    goto durwaun;
-  }
-  else
-  {
-    goto lepargyraea;
-  }
-  durwaun:
-  {
-  }
-
-  goto chirognomically;
-  goto aristippus;
-  lepargyraea:
-  {
-  }
-
-  aristippus:
-  {
-  }
-
-  var_9 = 42;
-  goto orcas;
-  catawampous:
-  {
-  }
-
-  var_3 = 42 + ((((((var_3 * var_3) * (var_3 + 1)) * (var_3 + 2)) * (var_3 + 3)) * (var_3 + 4)) % 5);
-  goto hemadromometer;
-  orcas:
-  {
-  }
-
-  chirognomically:
-  {
-  }
-
-  goto undergarments;
-  goto polycarpic;
-  hemadromometer:
-  {
-  }
-
-  polycarpic:
-  {
-  }
-
-  var_7 = (42 + ((((var_9 * var_9) * (var_9 + 1)) * (var_9 + 2)) % 3)) + ((((((var_7 * var_7) * (var_7 + 1)) * (var_7 + 2)) * (var_7 + 3)) * (var_7 + 4)) % 5);
-  desulfurs:
-  {
-  }
-
-  undergarments:
-  {
-  }
-
-  var_7 = 42;
-  var_7 = 42;
-  var_3 = 42 + ((((var_3 * var_3) * (var_3 + 1)) * (var_3 + 2)) % 3);
-  var_7 = 42 + ((((((var_13 * var_13) * (var_13 + 1)) * (var_13 + 2)) * (var_13 + 3)) * (var_13 + 4)) % 5);
-  var_8 = 42 + (((((var_13 * var_13) * (var_13 + 1)) * (var_13 + 2)) * (var_13 + 3)) % 4);
-  var_7 = 42;
 }
 
